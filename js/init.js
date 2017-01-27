0af173070ce8f24f6a19a008726ad6d4c16e01ce
Ryan Harper <spongery2@gmail.com>
2017-01-26 17:35:34 -0700

    New pages and additions, link changes

--- js/init.js
+++ js/init.js
@@ -2,6 +2,10 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    Materialize.updateTextFields();
+    $('select').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
